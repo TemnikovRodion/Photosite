4646ee8128c32f8a172597e21a24c42c3bff4a2d
Rodion Temnikov <rodion.temnikovv@gmail.com>
2021-03-04 03:56:53 +0300

    Updates

---
+++ precache-manifest.f3de234976aa4895c08e48abaa7e7041.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "805b362fefd6b9f7f00e7346238c58bb",
+    "url": "/Photosite/index.html"
+  },
+  {
+    "revision": "184d2dba42bb7ce186f7",
+    "url": "/Photosite/static/css/2.8c15c49b.chunk.css"
+  },
+  {
+    "revision": "727628fa7165039cfd84",
+    "url": "/Photosite/static/css/main.4d3bbe1a.chunk.css"
+  },
+  {
+    "revision": "184d2dba42bb7ce186f7",
+    "url": "/Photosite/static/js/2.e138772e.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/Photosite/static/js/2.e138772e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "727628fa7165039cfd84",
+    "url": "/Photosite/static/js/main.1de67ca1.chunk.js"
+  },
+  {
+    "revision": "0ed1c0b9b148ec9b3290",
+    "url": "/Photosite/static/js/runtime-main.b95067c9.js"
+  },
+  {
+    "revision": "bb75dde225ce651777b607ce07944846",
+    "url": "/Photosite/static/media/about.bb75dde2.jpg"
+  },
+  {
+    "revision": "6005cd24b8d3188d35dc5e033beeebfc",
+    "url": "/Photosite/static/media/icons.6005cd24.svg"
+  },
+  {
+    "revision": "2dd1118bcec4bf028c43266b12d4654f",
+    "url": "/Photosite/static/media/introduce.2dd1118b.jpg"
+  },
+  {
+    "revision": "605b1955f137c5a0f5c8bb9ef8e159a0",
+    "url": "/Photosite/static/media/montserrat-all-300-normal.605b1955.woff"
+  },
+  {
+    "revision": "7bf99c007acd1baa1f21903b6fda4d65",
+    "url": "/Photosite/static/media/montserrat-all-400-normal.7bf99c00.woff"
+  },
+  {
+    "revision": "3117c2d16f1e8cd7221d7c425a9b8c8e",
+    "url": "/Photosite/static/media/montserrat-all-600-normal.3117c2d1.woff"
+  },
+  {
+    "revision": "6ef75aa3d31900d5526ef77fe56fab11",
+    "url": "/Photosite/static/media/montserrat-cyrillic-300-normal.6ef75aa3.woff2"
+  },
+  {
+    "revision": "298b48e6dfdf7ec4ef8907ff8a7182b1",
+    "url": "/Photosite/static/media/montserrat-cyrillic-400-normal.298b48e6.woff2"
+  },
+  {
+    "revision": "67b4a56c5d3c35db36c3c7482886f7f2",
+    "url": "/Photosite/static/media/montserrat-cyrillic-600-normal.67b4a56c.woff2"
+  },
+  {
+    "revision": "709dc8a66a2f47013f721c2c3ab7a62a",
+    "url": "/Photosite/static/media/montserrat-cyrillic-ext-300-normal.709dc8a6.woff2"
+  },
+  {
+    "revision": "a13ef88d6cc2bace52bc1684ecb8823d",
+    "url": "/Photosite/static/media/montserrat-cyrillic-ext-400-normal.a13ef88d.woff2"
+  },
+  {
+    "revision": "64b7fb10b9b7d6a7c12375fc507cdff7",
+    "url": "/Photosite/static/media/montserrat-cyrillic-ext-600-normal.64b7fb10.woff2"
+  },
+  {
+    "revision": "7c3daf12b706645b5d3710f863a4da04",
+    "url": "/Photosite/static/media/montserrat-latin-300-normal.7c3daf12.woff2"
+  },
+  {
+    "revision": "bc3aa95dca08f5fee5291e34959c27bc",
+    "url": "/Photosite/static/media/montserrat-latin-400-normal.bc3aa95d.woff2"
+  },
+  {
+    "revision": "6fb1b5623e528e27c18658fecf5ee0ee",
+    "url": "/Photosite/static/media/montserrat-latin-600-normal.6fb1b562.woff2"
+  },
+  {
+    "revision": "de211e1120a6c4bd3b63b4b3ad7af4dd",
+    "url": "/Photosite/static/media/montserrat-latin-ext-300-normal.de211e11.woff2"
+  },
+  {
+    "revision": "97638b9a50d86ae960e84043314bf07a",
+    "url": "/Photosite/static/media/montserrat-latin-ext-400-normal.97638b9a.woff2"
+  },
+  {
+    "revision": "f44682aac1828f936e6d11685aa391c8",
+    "url": "/Photosite/static/media/montserrat-latin-ext-600-normal.f44682aa.woff2"
+  },
+  {
+    "revision": "f20ef77d1f16847f56628b0099f3987e",
+    "url": "/Photosite/static/media/montserrat-vietnamese-300-normal.f20ef77d.woff2"
+  },
+  {
+    "revision": "8c255c10d88cb2f73b25cad652183ad4",
+    "url": "/Photosite/static/media/montserrat-vietnamese-400-normal.8c255c10.woff2"
+  },
+  {
+    "revision": "47a748dbb9353b6899c5c37d93fe42cc",
+    "url": "/Photosite/static/media/montserrat-vietnamese-600-normal.47a748db.woff2"
+  },
+  {
+    "revision": "80a1518fa46a355afb36f4e000db509e",
+    "url": "/Photosite/static/media/parisienne-all-400-normal.80a1518f.woff"
+  },
+  {
+    "revision": "f8643e2cb5f9731e787aeeee3d99f932",
+    "url": "/Photosite/static/media/parisienne-latin-400-normal.f8643e2c.woff2"
+  },
+  {
+    "revision": "6f4c5e64193b524f0e5c6ee64d95c025",
+    "url": "/Photosite/static/media/parisienne-latin-ext-400-normal.6f4c5e64.woff2"
+  }
+]);
